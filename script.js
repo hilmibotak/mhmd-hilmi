acea46b1c60ee74bc5467410ec8b6183a79c8cc4
hilmibotak <147034906+hilmibotak@users.noreply.github.com>
2023-11-30 15:17:40 +0700

    footer

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ function mobil() {
     document.getElementById("RentalMobil").src='gambar/mobil_2-removebg-preview.png'
   }
  
+  function newTab(){
+    open.window('https://tailwindcss.com/')
+  }
