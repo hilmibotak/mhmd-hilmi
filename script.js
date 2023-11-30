2e7deb9254c19aa0d97a7e36f7ca4de72b9792d6
hilmibotak <147034906+hilmibotak@users.noreply.github.com>
2023-11-30 14:56:03 +0700

    men

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 function mobil() {
     document.getElementById("RentalMobil").src='gambar/mobil_2-removebg-preview.png'
   }
+  function bukaLink() {
+    document.getElementById(Kontak kami)href = "https://www..com";
+  }
